ec5e0647b0d71c445b84a22807225722e3467a08
Shodhan Shetty <Shodhanshetty144@gmail.com>
2022-11-07 20:52:54 +0530

    Added title

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-red-500 text-3xl text-center'>Love Calculator</div>
+    <div className='text-pink-500 text-3xl text-center'>Love Calculator</div>
   )
 }
