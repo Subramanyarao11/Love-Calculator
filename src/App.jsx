f022a8078f0856624181e279a45a72ecc488f761
Shodhan Shetty <Shodhanshetty144@gmail.com>
2022-11-07 21:37:00 +0530

    added component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Calculator from './components/Calculator'
 
 export default function App() {
+
   return (
+    <>
     <div className='text-pink-500 text-3xl text-center'>Love Calculator</div>
+    <h1>We will build this tommorrow!</h1>
+    <Calculator/>
+    </>
   )
 }
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Calculator() {
+  return (
+    <div>
+      
+      We will be doing Api stuffs tommorrow.
+    
+    </div>
+  )
+}
